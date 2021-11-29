55fcc70180c404c901512e56c7323fdecc53407f
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-11-29 12:31:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
-function start() {
+function btn_start() {
     document.getElementById("textbox").innerHTML = "";
-    console.log("hello");
     recognition.start();
+    Webcam.attach(camera);
 }
 
 recognition.onresult() = function(event){
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speakData = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+    synth.speak(utterThis);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: "jpeg",
+    jpeg_quality: 90
+});
+
+var camera = document.getElementById("camera");
