518b29465db3e77c76ad9009dd9c2ba65700d0da
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-12-03 12:40:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,17 +14,38 @@ Webcam.set({
 
 var camera = document.getElementById("camera");
 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    if (content == "take my selfie") {
+        speak();
+    }
+
     Webcam.attach(camera);
 }
 
 function speak() {
     var synth = window.speechSynthesis;
-    var speakData = document.getElementById("textbox").value;
+    //var speakData = document.getElementById("textbox").value;
+    speakData = "Taking your selfie in 5 seconds.";
     var utterThis = new SpeechSynthesisUtterance(speakData);
     synth.speak(utterThis);
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 5000);
+}
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id = 'selfie' src = " + data_uri + ">";
+    })
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie").src;
+    link.href = image;
+    link.click();
 }
